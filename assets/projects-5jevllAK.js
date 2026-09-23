416cb91b1aea16b8a2e4fb98f94a564dfbf8a8a2
Rubin Zhao <beanli161514@users.noreply.github.com>
2026-09-24 05:39:04 +0800

    Publish reviewed project pages and streamlined NeuroFly demo

---
+++ assets/projects-5jevllAK.js
@@ -0,0 +1,1 @@
+import"./site-DEEGMWxb.js";const e=new Set(["#experiment","#experiment-title","#shape","#shape-title","#method-title"]);e.has(window.location.hash)&&window.location.replace(`/ear/${window.location.search}${window.location.hash}`);
